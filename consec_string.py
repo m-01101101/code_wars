32641915252d73dcbc170b2d7dac85c854b1bf5d
michael <michaelrogers.arsenal@gmail.com>
2020-05-25 16:32:19 +0100

    consec_strings

---
+++ consec_string.py
@@ -0,0 +1,10 @@
+def longest_consec(strarr: list, k: int) -> str:
+    strs = []
+    if k > len(strarr) or k <= 0:
+        return ''
+    else:
+        for i in range(len(strarr)):
+            strs.append(''.join(strarr[i:k]))
+            k += 1
+        strs.sort(key=len, reverse=True)
+        return strs[0]
